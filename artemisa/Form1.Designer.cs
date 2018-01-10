0efe82d949f184ac377ac200cd637117fcd2a0ef
naivenom <naivenom@protonmail.com>
2018-01-10 23:40:29 +0100

    Add files via upload

---
+++ artemisa/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace artemisa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipAddress = new System.Windows.Forms.TextBox();
+            this.stop = new System.Windows.Forms.Button();
+            this.scan = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OutPut = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.escanerDeRed = new System.Windows.Forms.RadioButton();
+            this.deteccion = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ipAddress
+            // 
+            this.ipAddress.Location = new System.Drawing.Point(672, 119);
+            this.ipAddress.Name = "ipAddress";
+            this.ipAddress.Size = new System.Drawing.Size(156, 22);
+            this.ipAddress.TabIndex = 0;
+            this.ipAddress.TextChanged += new System.EventHandler(this.ipAddress_TextChanged);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(753, 201);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 30);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // scan
+            // 
+            this.scan.Location = new System.Drawing.Point(672, 201);
+            this.scan.Name = "scan";
+            this.scan.Size = new System.Drawing.Size(75, 30);
+            this.scan.TabIndex = 3;
+            this.scan.Text = "scan";
+            this.scan.UseVisualStyleBackColor = true;
+            this.scan.Click += new System.EventHandler(this.scan_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(669, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Estado:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // OutPut
+            // 
+            this.OutPut.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.OutPut.Location = new System.Drawing.Point(4, 1);
+            this.OutPut.Name = "OutPut";
+            this.OutPut.Size = new System.Drawing.Size(342, 390);
+            this.OutPut.TabIndex = 1;
+            this.OutPut.UseCompatibleStateImageBehavior = false;
+            this.OutPut.View = System.Windows.Forms.View.Details;
+            this.OutPut.SelectedIndexChanged += new System.EventHandler(this.OutPut_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Direccion IP";
+            this.columnHeader1.Width = 95;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nombre del Host";
+            this.columnHeader2.Width = 180;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Estatus";
+            this.columnHeader3.Width = 64;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(366, 368);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(558, 23);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::artemisa.Properties.Resources.artemisa;
+            this.pictureBox1.Location = new System.Drawing.Point(352, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(572, 312);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // escanerDeRed
+            // 
+            this.escanerDeRed.AutoSize = true;
+            this.escanerDeRed.Location = new System.Drawing.Point(672, 174);
+            this.escanerDeRed.Name = "escanerDeRed";
+            this.escanerDeRed.Size = new System.Drawing.Size(131, 21);
+            this.escanerDeRed.TabIndex = 8;
+            this.escanerDeRed.TabStop = true;
+            this.escanerDeRed.Text = "Escaner de Red";
+            this.escanerDeRed.UseVisualStyleBackColor = true;
+            // 
+            // deteccion
+            // 
+            this.deteccion.AutoSize = true;
+            this.deteccion.Location = new System.Drawing.Point(672, 147);
+            this.deteccion.Name = "deteccion";
+            this.deteccion.Size = new System.Drawing.Size(250, 21);
+            this.deteccion.TabIndex = 9;
+            this.deteccion.TabStop = true;
+            this.deteccion.Text = "Deteccion OS y Usuario (Windows)";
+            this.deteccion.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(933, 403);
+            this.Controls.Add(this.deteccion);
+            this.Controls.Add(this.escanerDeRed);
+            this.Controls.Add(this.OutPut);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.scan);
+            this.Controls.Add(this.ipAddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "artemisa";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ipAddress;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button scan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView OutPut;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.RadioButton escanerDeRed;
+        private System.Windows.Forms.RadioButton deteccion;
+    }
+}
+
---
+++ artemisa/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Net;
+using System.Management;
+
+namespace artemisa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.ForeColor = System.Drawing.Color.Red;
+            label1.Text = "Parado";
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        Thread myThread = null;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void _scan(string subred)
+        {
+            Ping _ping;
+            PingReply _respuesta;
+            IPAddress _ip;
+            IPHostEntry _host;
+
+            progressBar1.Maximum = 254;
+            progressBar1.Value = 0;
+            OutPut.Items.Clear();
+
+            for (int i = 1; i < 255; i++)
+            {
+                string _subred = "." + i.ToString();
+                _ping = new Ping();
+                _respuesta = _ping.Send(subred + _subred, 900);
+
+                label1.ForeColor = System.Drawing.Color.Green;
+                label1.Text = "Escaneando: " + subred + _subred;
+
+                if (_respuesta.Status == IPStatus.Success)
+                {
+                    try
+                    {
+                        _ip = IPAddress.Parse(subred + _subred);
+                        _host = Dns.GetHostEntry(_ip);
+
+                        OutPut.Items.Add(new ListViewItem(new String[] { subred + _subred, _host.HostName, "Up" }));
+                    }
+                    catch { OutPut.Items.Add(new ListViewItem(new String[] { subred + _subred, "Probablemente *nix", "Up" })); }
+                }
+                progressBar1.Value += 1;
+            }
+            scan.Enabled = true;
+            stop.Enabled = false;
+            ipAddress.Enabled = true;
+            label1.Text = "Realizado con exito!";
+            int _contador = OutPut.Items.Count;
+            MessageBox.Show("Escaneo realizado!\nEncontrados " + _contador.ToString() + " hosts.", "Hecho", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+        }
+
+        public void _detallesWin(string _host)
+        {
+            string con = null;
+            string[] _deteccionWin = { "Win32_ComputerSystem", "Win32_OperatingSystem" };
+            string[] param = { "UserName", "Caption" };
+
+            label1.ForeColor = System.Drawing.Color.Green;
+
+            for (int i = 0; i <= _deteccionWin.Length - 1; i++)
+            {
+                label1.Text = "Obteniendo informacion.";
+                try
+                {
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher("\\\\" + _host + "\\root\\CIMV2", "SELECT *FROM " + _deteccionWin[i]);
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        label1.Text = "Obteniendo informacion. .";
+
+                        con += obj.GetPropertyValue(param[i]).ToString() + "\n";
+                        if (i == _deteccionWin.Length - 1)
+                        {
+                            label1.Text = "Hecho!";
+                            MessageBox.Show(con, "Host Windows: " + _host, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            break;
+                        }
+                        label1.Text = "Obteniendo informacion. . .";
+                    }
+                }
+                catch (Exception excepcion) { MessageBox.Show("Error en consulta WMI.\n\n" + excepcion.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); break; }
+            }
+        }
+
+        private void scan_Click(object sender, EventArgs e)
+        {
+            if (ipAddress.Text == string.Empty)
+            {
+                MessageBox.Show("No se ha introducido ninguna IP.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (escanerDeRed.Checked)
+                {
+                    myThread = new Thread(() => _scan(ipAddress.Text));
+                    myThread.Start();
+
+                    if (myThread.IsAlive == true)
+                    {
+                        stop.Enabled = true;
+                        scan.Enabled = false;
+                        ipAddress.Enabled = false;
+                    }
+                }
+                if (deteccion.Checked)
+                {
+                    Thread qThread = new Thread(() => _detallesWin(ipAddress.Text));
+                    qThread.Start();
+
+                    if (qThread.IsAlive == true)
+                    {
+                        stop.Enabled = true;
+                        scan.Enabled = false;
+                        ipAddress.Enabled = false;
+                    }
+                }
+                
+            }
+        }
+
+        private void ipAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            myThread.Suspend();
+            scan.Enabled = true;
+            stop.Enabled = false;
+            ipAddress.Enabled = true;
+            label1.ForeColor = System.Drawing.Color.Red;
+            label1.Text = "Parado";
+        }
+
+        private void OutPut_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
+        
+    }
+}
